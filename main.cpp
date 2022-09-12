3694b9d01166070ba83a5ec634ec55c5fc452a2c
Aviansh <56665437+froster04@users.noreply.github.com>
2022-09-12 08:49:35 +0545

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,180 @@
+#include "raylib.h"
+
+struct Ball
+{
+	float x, y;
+	float speedX, speedY;
+	float radius;
+
+	void Draw()
+	{
+		DrawCircle((int)x, (int)y, radius, GREEN);
+	}
+};
+
+struct Paddle
+{
+	float x, y;
+	float speed;
+	float width, height;
+
+	Rectangle GetRect()
+	{
+		return Rectangle{ x - width / 2, y - height / 2, width, height };
+	}
+
+	void Draw() {
+		DrawRectangleRec(GetRect(), WHITE);
+	}
+};
+
+
+
+int main() {
+	//Window Initilizaion
+	InitWindow(800, 600, "Pong");
+	SetWindowState(FLAG_VSYNC_HINT);
+	InitAudioDevice();
+	//Game Objects 
+	Ball ball;
+	Paddle leftPaddle;
+	Paddle rightPaddle;
+	
+	//Ball
+	ball.x= GetScreenHeight() / 2.0f;
+	ball.y = GetScreenHeight() / 2.0f;
+	ball.radius = 10;
+	ball.speedX = 300;
+	ball.speedY = 300;
+	
+	//Paddle
+	leftPaddle.x = 50;
+	leftPaddle.y = GetScreenHeight() / 2;
+	leftPaddle.width = 10;
+	leftPaddle.height = 100;
+	leftPaddle.speed = 500;
+
+	rightPaddle.x = GetScreenWidth() - 50;
+	rightPaddle.y = GetScreenHeight() / 2;
+	rightPaddle.width = 10;
+	rightPaddle.height = 100;
+	rightPaddle.speed = 500;
+
+	Sound sound = LoadSound("Resources/sound.wav");
+	const char* winnerText = nullptr;
+
+	//Gameloop
+	while (!WindowShouldClose())
+	{
+		ball.x += ball.speedX * GetFrameTime();
+		ball.y += ball.speedY * GetFrameTime();
+
+		if (ball.y < 0)
+		{
+			ball.y = 0;
+			ball.speedY *= -1;
+		}
+		if (ball.y > GetScreenHeight())
+		{
+			ball.y = GetScreenHeight();
+			ball.speedY *= -1;
+		}
+
+
+
+		//Paddle Movement
+		if (IsKeyDown(KEY_W))
+		{
+			leftPaddle.y -= leftPaddle.speed * GetFrameTime();
+		}
+		if (IsKeyDown(KEY_S))
+		{
+			leftPaddle.y += leftPaddle.speed * GetFrameTime();
+		}
+
+		if (IsKeyDown(KEY_UP))
+		{
+			rightPaddle.y -= rightPaddle.speed * GetFrameTime();
+		}
+		if (IsKeyDown(KEY_DOWN))
+		{
+			rightPaddle.y += rightPaddle.speed * GetFrameTime();
+		}
+
+		/*if (IsKeyPressed(KEY_R))
+		{
+			ball.x = GetScreenWidth() / 2;
+			ball.y = GetScreenHeight() / 2;
+			ball.speedX = 300;
+		}*/
+
+		//Collision detection
+		if (CheckCollisionCircleRec(Vector2{ ball.x,ball.y }, ball.radius, leftPaddle.GetRect()))
+		{
+			if (ball.speedX < 0)
+			{
+			ball.speedX *= -1.1f;
+			ball.speedY = (ball.y - leftPaddle.y) / (leftPaddle.height / 2) * ball.speedX;
+			PlaySound(sound);
+			}
+		}
+		if (CheckCollisionCircleRec(Vector2{ ball.x,ball.y }, ball.radius, rightPaddle.GetRect()))
+		{
+			if (ball.speedX > 0)
+			{
+			ball.speedX *= -1.1f;
+			ball.speedY = (ball.y - rightPaddle.y) / (rightPaddle.height / 2) * - ball.speedX;
+			PlaySound(sound);
+			}
+		}
+
+		//Winner Checking
+		if (ball.x < 0)
+		{
+			winnerText = "Right Player Wins!";
+			DrawText("Press R to reset", 100, 10, 20, WHITE);
+		}
+		if (ball.x > GetScreenWidth())
+		{
+			winnerText = "Left Player Wins!";
+			DrawText("Press R to reset", 100, 10, 20, WHITE);
+		}
+		
+		//Restart Game
+		if (winnerText && IsKeyPressed(KEY_R))
+		{
+			ball.x = GetScreenWidth() / 2;
+			ball.y = GetScreenHeight() / 2;
+			ball.speedX = 300;
+			ball.speedY = 300;
+
+			leftPaddle.x = 50;
+			leftPaddle.y = GetScreenHeight() / 2;
+
+			rightPaddle.x = GetScreenWidth() - 50;
+			rightPaddle.y = GetScreenHeight() / 2;
+
+			winnerText = nullptr;
+
+		}
+
+		BeginDrawing();
+		ClearBackground(BLACK);
+
+		ball.Draw();
+		rightPaddle.Draw();
+		leftPaddle.Draw();
+		
+		if (winnerText)
+		{
+			int textWidh = MeasureText(winnerText, 60);
+			DrawText(winnerText, GetScreenWidth() / 2 - textWidh / 2, GetScreenHeight() / 2 - 30, 60, YELLOW);
+		}
+
+		DrawFPS(10, 10);
+		EndDrawing();
+	}
+	CloseAudioDevice();
+	CloseWindow();
+	return 0;
+}
